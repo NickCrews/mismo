e94c7b23d34b04323ed0dae50e8a3d0a06eebc49
Nick Crews <nicholas.b.crews@gmail.com>
2022-06-11 22:19:05 -0600

    Use different fingerprinters for both datasets
    One dataset might not be the same schema as the other, or you might
    want to generate the fingerprint key differently for each

--- mismo/block/blocker.py
+++ mismo/block/blocker.py
@@ -1,4 +1,4 @@
-from typing import Protocol, Sequence
+from typing import Iterable, Protocol
 
 import modin.pandas as pd
 
@@ -11,28 +11,55 @@ class PBlocker(Protocol):
         ...
 
 
-class FingerprintBlocker(PBlocker):
-    fingerprinters: list[PFingerprinter]
+FingerprinterPair = tuple[PFingerprinter, PFingerprinter]
+FingerprinterPairsLike = Iterable[FingerprinterPair]
 
-    def __init__(
-        self, fingerprinters: Sequence[PFingerprinter] | PFingerprinter
-    ) -> None:
-        if hasattr(fingerprinters, "fingerprint"):
-            self.fingerprinters = [fingerprinters]  # type: ignore
-        else:
-            self.fingerprinters = list(fingerprinters)  # type: ignore
+
+class FingerprintBlocker(PBlocker):
+    fingerprinters: list[FingerprinterPair]
+
+    def __init__(self, fingerprinters: FingerprinterPairsLike) -> None:
+        self.fingerprinters = self._convert_fingerprinters(fingerprinters)
 
     def block(self, data1: Data, data2: Data) -> Links:
         link_chunks = []
-        for fp in self.fingerprinters:
-            prints1 = fp.fingerprint(data1)
-            prints2 = fp.fingerprint(data2)
+        for fp1, fp2 in self.fingerprinters:
+            prints1 = fp1.fingerprint(data1)
+            prints2 = fp2.fingerprint(data2)
             link_chunk = merge_fingerprints(prints1, prints2)
             link_chunks.append(link_chunk)
         links: pd.DataFrame = pd.concat(link_chunks)
         links.drop_duplicates(inplace=True)
         links.sort_values(by=["index_1", "index_2"], inplace=True)
         return links
+
+    @staticmethod
+    def _is_fingerprinter(fp):
+        return hasattr(fp, "fingerprint")
+
+    @classmethod
+    def _convert_fingerprinters(
+        cls, fps: FingerprinterPairsLike
+    ) -> list[FingerprinterPair]:
+        fps_list = list(fps)
+        if not fps_list:
+            raise ValueError("Fingerprinters must not be empty")
+        result = []
+        for fp in fps_list:
+            pair = tuple(fp)
+            if not len(pair) == 2:
+                raise ValueError(
+                    "Fingerprinters must be a sequence of length 2. ",
+                    f"Got {pair}",
+                )
+            if not cls._is_fingerprinter(pair[0]) or not cls._is_fingerprinter(pair[1]):
+                raise ValueError(
+                    "Fingerprinters must be instances of Fingerprinter. ",
+                    f"Got {pair}",
+                )
+            result.append(pair)
+        # mypy doesn't understand that pair is length 2.
+        return result  # type: ignore
 
 
 def merge_fingerprints(fp1: pd.DataFrame, fp2: pd.DataFrame) -> Links:
