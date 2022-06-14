7e2fc39dbcd31e081f1a52bda76033692806d33a
Nick Crews <nicholas.b.crews@gmail.com>
2022-06-13 21:30:53 -0600

    Set dtypes in example data

--- mismo/datasets.py
+++ mismo/datasets.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import numpy as np
 import pandas as pd
 from recordlinkage.datasets import load_febrl1 as _load_febrl1
 
@@ -8,6 +9,19 @@ def load_febrl1() -> tuple[pd.DataFrame, pd.DataFrame]:
     df, links = _load_febrl1(return_links=True)
     index_iloc_mapping = {idx: i for i, idx in enumerate(df.index)}
     df.index = range(len(df))
+    dtypes = {
+        "given_name": "string[pyarrow]",
+        "surname": "string[pyarrow]",
+        "street_number": "string[pyarrow]",  # keep as string for leading 0s
+        "address_1": "string[pyarrow]",
+        "address_2": "string[pyarrow]",
+        "suburb": "string[pyarrow]",
+        "postcode": "string[pyarrow]",  # keep as string for leading 0s
+        "state": "string[pyarrow]",
+        "soc_sec_id": np.int32,  # 7 digits long, never null
+        "date_of_birth": "string[pyarrow]",  # contains some BS dates like 19371233
+    }
+    df = df.astype(dtypes)
     link_df: pd.DataFrame = links.to_frame()
     link_df = link_df.reset_index(drop=True)
     link_df.columns = ["index_left", "index_right"]
