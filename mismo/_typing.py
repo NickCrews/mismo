db889f297f0cc21c852b8be173620342ca5d6a09
Nick Crews <nicholas.b.crews@gmail.com>
2023-02-26 14:13:12 -0900

    Remove unused _typing.py

--- mismo/_typing.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from typing import TypeVar
-
-# Little hack because mypy doesn't support
-# Self types from either the stdlib or from typing_extensions
-# https://github.com/python/mypy/issues/11871
-Self = TypeVar("Self", bound="object")
