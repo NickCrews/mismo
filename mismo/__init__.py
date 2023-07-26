68e129dd42df4bde68a710e1726cbab9f5b7a144
Nick Crews <nicholas.b.crews@gmail.com>
2023-07-26 11:06:57 -0800

    Remove unused _dedupe.py
    I need the rest of the API to mature enough before
    I try to compose everything together into this
    one helper class.

--- mismo/__init__.py
+++ mismo/__init__.py
@@ -3,4 +3,3 @@ from __future__ import annotations
 
 from mismo._dataset import DedupeDatasetPair as DedupeDatasetPair
 from mismo._dataset import LinkageDatasetPair as LinkageDatasetPair
-from mismo._dedupe import Deduper as Deduper
