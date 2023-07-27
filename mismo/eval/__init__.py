0be399039d3c0ccc57a48e92cbc24c43c80e6ad1
Nick Crews <nicholas.b.crews@gmail.com>
2023-07-26 22:20:10 -0800

    rename metrics/ to eval/

---
+++ mismo/eval/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import annotations
+
+from mismo.eval._block import reduction_ratio as reduction_ratio
--- mismo/metrics/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-from mismo.metrics._block import reduction_ratio  # noqa: F401
