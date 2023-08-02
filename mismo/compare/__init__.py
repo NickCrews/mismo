b0bbaddbfe56085db8cce93b5b3367fd8cfdf8f6
Nick Crews <nicholas.b.crews@gmail.com>
2023-08-02 13:54:08 -0800

    Add distance_km() comparison method

--- mismo/compare/__init__.py
+++ mismo/compare/__init__.py
@@ -10,6 +10,7 @@ from ._array import intersection_n as intersection_n
 from ._array import jaccard as jaccard
 from ._comparison import Comparison as Comparison
 from ._comparison import ComparisonLevel as ComparisonLevel
+from ._geospatial import distance_km as distance_km
 from ._levels import exact_level as exact_level
 
 __all__ = [
@@ -19,4 +20,5 @@ __all__ = [
     "jaccard",
     "exact_level",
     "fs",
+    "distance_km",
 ]
---
+++ mismo/compare/_geospatial.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import math
+
+from ibis.expr.types import FloatingValue
+
+
+def distance_km(
+    *,
+    lat1: FloatingValue,
+    lon1: FloatingValue,
+    lat2: FloatingValue,
+    lon2: FloatingValue,
+) -> FloatingValue:
+    """The distance between two points on the Earth's surface, in kilometers.
+
+    Parameters
+    ----------
+    lat1 : FloatingValue
+        The latitude of the first point.
+    lon1 : FloatingValue
+        The longitude of the first point.
+    lat2 : FloatingValue
+        The latitude of the second point.
+    lon2 : FloatingValue
+        The longitude of the second point.
+    """
+
+    def radians(degrees):
+        scaling = math.pi / 180
+        return degrees * scaling
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    def haversine(theta: FloatingValue) -> FloatingValue:
+        return (theta / 2).sin() ** 2
+
+    a = haversine(lat2 - lat1) + lat1.cos() * lat2.cos() * haversine(lon2 - lon1)
+    c = 2 * a.sqrt().asin()
+    R_earth = 6371.0  # km
+    return R_earth * c
