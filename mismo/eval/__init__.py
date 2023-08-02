bb08b7033a839df5584f0ab282a384ea686bfe9b
Nick Crews <nicholas.b.crews@gmail.com>
2023-08-02 13:54:41 -0800

    Remove reduction_ratio() from eval, move to n_naive_comparisons()

--- mismo/eval/__init__.py
+++ mismo/eval/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
 
-from mismo.eval._block import reduction_ratio as reduction_ratio
+from mismo.eval._block import n_naive_comparisons as n_naive_comparisons
--- mismo/eval/_block.py
+++ mismo/eval/_block.py
@@ -2,16 +2,33 @@ from __future__ import annotations
 
 from typing import Sized, Union
 
-Sizable = Union[int, Sized]
+from ibis.common.exceptions import ExpressionError
+from ibis.expr.types import Table
+
+Sizable = Union[int, Sized, Table]
 
 
 def _get_len(x: Sizable) -> int:
     if isinstance(x, int):
         return x
-    else:
+    try:
         return len(x)
+    except (TypeError, ExpressionError):
+        return x.count().execute()
 
 
-def reduction_ratio(datal: Sizable, datar: Sizable, links: Sizable) -> float:
-    naive_links = (_get_len(datal) * _get_len(datar)) / 2
-    return 1 - _get_len(links) / naive_links
+def n_naive_comparisons(left: Sizable, right: Sizable | None = None) -> int:
+    """The number of comparisons if we compared every record to every other record.
+
+    Parameters
+    ----------
+    left : int | Sized | Table
+        The number of records in the left dataset, or the left dataset itself.
+    right : int | Sized | Table, optional
+        The number of records in the right dataset, or the right dataset itself.
+        For dedupe tasks leave this as None.
+    """
+    if right is None:
+        return _get_len(left) * (_get_len(left) - 1) // 2
+    else:
+        return _get_len(left) * _get_len(right)
