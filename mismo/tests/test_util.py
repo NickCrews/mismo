08cfa3294a11ff12c04dbe7f37900c98be08fef9
Nick Crews <nicholas.b.crews@gmail.com>
2023-08-01 16:40:41 -0800

    fixup: test: fix test_intify_column()
    not sure what broke this

--- mismo/tests/test_util.py
+++ mismo/tests/test_util.py
@@ -8,13 +8,13 @@ from mismo import _util
 def test_intify_column():
     inp = ibis.memtable(
         {
-            "x": ["b", "b", "a", "c", None],
-            "ints": [1, 1, 0, 2, 3],
+            "vals": ["b", "b", "a", "c", None],
+            "ints_expected": [1, 1, 0, 2, 3],
         }
     )
-    inp = inp.mutate(y=inp.x)
-    inted, restore = _util.intify_column(inp, "x")
-    assert inted.x.type() == ibis.literal(4, type="uint64").type()
-    assert (inted.x == inted.ints_expected).all().execute()
+    inp = inp.mutate(original=inp.vals)
+    inted, restore = _util.intify_column(inp, "vals")
+    assert inted.vals.type() == ibis.literal(4, type="uint64").type()
+    assert (inted.vals == inted.ints_expected).all().execute()
     restored = restore(inted)
-    assert (restored.x == restored.original).all().execute()
+    assert (restored.vals == restored.original).all().execute()
