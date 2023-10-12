36c6a005cc6c0b44c143c8bbaed1d5bcbfbb2039
Nick Crews <nicholas.b.crews@gmail.com>
2023-10-12 14:34:19 -0800

    fix(block): fix renaming when using equi-joins

--- mismo/block/_util.py
+++ mismo/block/_util.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from ibis import _
 from ibis.expr.types import Table
 
 from mismo import _util
@@ -31,6 +32,18 @@ def join(left: Table, right: Table, condition) -> Table:
     right_renaming = {c + "_r": c for c in just_right}
     renaming = {**left_renaming, **right_renaming}
     result = raw.rename(renaming)
+
+    # If the condition is an equality condition, like `left.name == right.name`,
+    # then since we are doing an inner join ibis doesn't add suffixes to these
+    # columns. So we need duplicate these columns and add suffixes.
+    un_suffixed = [
+        c for c in result.columns if not c.endswith("_l") and not c.endswith("_r")
+    ]
+    suffix_map = {c + "_l": _[c] for c in un_suffixed} | {
+        c + "_r": _[c] for c in un_suffixed
+    }
+    result = result.mutate(**suffix_map).drop(*un_suffixed)
+
     return order_blocked_data_columns(result)
 
 
