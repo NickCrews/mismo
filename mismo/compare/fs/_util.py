6ed1edd41afd49e47ca5f6e9466f256972e4474b
Nick Crews <nicholas.b.crews@gmail.com>
2023-11-03 22:50:15 -0600

    fix(fs/util): make prob conversion utils work with more ibis types
    before they didn't work correctly with Deferreds and Int/Decimal types

--- mismo/compare/fs/_util.py
+++ mismo/compare/fs/_util.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
 import math
-from typing import overload
+from typing import Union, overload
 
 import ibis
-from ibis.expr.types import FloatingValue
+from ibis.expr.deferred import Deferred
+from ibis.expr.types import NumericValue
+
+_IbisIsh = Union[Deferred, NumericValue]
 
 
 @overload
@@ -13,13 +16,13 @@ def prob_to_odds(prob: float) -> float:
 
 
 @overload
-def prob_to_odds(prob: FloatingValue) -> FloatingValue:
+def prob_to_odds(prob: _IbisIsh) -> _IbisIsh:
     ...
 
 
 def prob_to_odds(prob):
     normal = prob / (1 - prob)
-    if isinstance(prob, FloatingValue):
+    if isinstance(prob, _IbisIsh):
         return ibis.ifelse(prob != 1, normal, ibis.literal(float("inf")))
     else:
         return normal if prob != 1 else float("inf")
@@ -31,13 +34,13 @@ def odds_to_prob(odds: float) -> float:
 
 
 @overload
-def odds_to_prob(odds: FloatingValue) -> FloatingValue:
+def odds_to_prob(odds: _IbisIsh) -> _IbisIsh:
     ...
 
 
 def odds_to_prob(odds):
     normal = odds / (1 + odds)
-    if isinstance(odds, FloatingValue):
+    if isinstance(odds, _IbisIsh):
         return ibis.ifelse(odds.isinf(), 1.0, normal)
     else:
         return 1 if odds == float("inf") else normal
@@ -49,12 +52,12 @@ def odds_to_log_odds(odds: float) -> float:
 
 
 @overload
-def odds_to_log_odds(odds: FloatingValue) -> FloatingValue:
+def odds_to_log_odds(odds: _IbisIsh) -> _IbisIsh:
     ...
 
 
 def odds_to_log_odds(odds):
-    if isinstance(odds, FloatingValue):
+    if isinstance(odds, _IbisIsh):
         return odds.log10()
     else:
         if odds == 0:
