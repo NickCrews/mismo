59ae742de900290e0c3da1b886537c7f41f9f8e1
Nick Crews <nicholas.b.crews@gmail.com>
2023-12-06 21:53:07 -0900

    types: jaccard() returns FloatingValue not IntegerValue

--- mismo/compare/_array.py
+++ mismo/compare/_array.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from ibis.expr.types import ArrayValue, IntegerValue
+from ibis.expr.types import ArrayValue, FloatingValue
 
 
-def jaccard(a: ArrayValue, b: ArrayValue) -> IntegerValue:
+def jaccard(a: ArrayValue, b: ArrayValue) -> FloatingValue:
     """The Jaccard similarity between two arrays.
 
     Parameters
@@ -15,7 +15,7 @@ def jaccard(a: ArrayValue, b: ArrayValue) -> IntegerValue:
 
     Returns
     -------
-    IntegerValue
+    FloatingValue
         The Jaccard similarity between the two arrays.
     """
     intersection = a.intersect(b).length()
