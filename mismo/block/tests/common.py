5edc29cf24e91e4dddd5343f6b51428009c2d785
Nick Crews <nicholas.b.crews@gmail.com>
2024-02-08 14:06:50 -0900

    test: add more columns to t1 and t2 fixtures

--- mismo/block/tests/common.py
+++ mismo/block/tests/common.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
 
-def letters_blocked(table_factory):
+def letters_blocked_ids(table_factory):
     """If you block the fixtures t1 and t2 on the letters column,
-    this is what you should get."""
+    these are the record_ids what you should get."""
     return table_factory(
         {
             "record_id_l": [1, 2],
             "record_id_r": [90, 91],
-            "letters_l": ["b", "c"],
-            "letters_r": ["b", "c"],
         }
     )
--- mismo/tests/util.py
+++ mismo/tests/util.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 from ibis.expr.types import Table
+import pandas._testing as tm
 
 
 def assert_tables_equal(left: Table, right: Table) -> None:
     assert left.schema() == right.schema()
     left_df = left.order_by(left.columns).to_pandas()
     right_df = right.order_by(left.columns).to_pandas()
-    assert left_df.equals(right_df)
+    tm.assert_frame_equal(left_df, right_df)
