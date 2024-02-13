1c4e438bc3d445e1b6166fcd327da5666e3457d0
Nick Crews <nicholas.b.crews@gmail.com>
2024-02-12 22:20:04 -0900

    fixup: generate name of BlockingRule correctly

--- mismo/block/_blocking.py
+++ mismo/block/_blocking.py
@@ -170,15 +170,13 @@ class BlockingRule:
             This can be any of the following:
 
             - anything that ibis accepts as a join predicate
-            - tuple[str, str]
-            - tuple[Column, Column]
-            - tuple[Deferred, Deferred]
             - A callable that takes two tables and returns any of the above
         name
-            The name of the rule.
+            The name of the rule. If not provided, a name will be generated based on
+            the condition.
         """
         self._condition = condition
-        self._name = blocker_name(condition)
+        self._name = name if name is not None else blocker_name(condition)
 
     def get_name(self) -> str:
         """The name of the rule."""
@@ -197,7 +195,6 @@ class BlockingRule:
         on_slow: Literal["error", "warn", "ignore"] = "error",
         **kwargs,
     ) -> Table:
-        # return _util.join(left, right, self.condition, on_slow=on_slow)
         return self.condition
 
     def block(self, left: Table, right: Table, **kwargs) -> Table:
@@ -205,86 +202,3 @@ class BlockingRule:
 
     def __repr__(self) -> str:
         return f"BlockingRule({self.get_name()})"
-
-
-# class BlockingRules:
-#     """An unordered, dict-like collection of `BlockingRule`s"""
-
-#     def __init__(self, *rules: BlockingRule) -> None:
-#         """Create a new collection of blocking rules.
-
-#         Each rule must have a unique name.
-#         """
-#         self._lookup: dict[str, BlockingRule] = {}
-#         for rule in rules:
-#             if rule.name in self._lookup:
-#                 raise ValueError(f"Duplicate Rule name: {rule.name}")
-#             self._lookup[rule.name] = rule
-
-#     def __call__(
-#         self,
-#         left: Table,
-#         right: Table,
-#         *,
-#         on_slow: Literal["error", "warn", "ignore"] = "error",
-#         labels: bool = False,
-#         **kwargs,
-#     ) -> Table:
-#         """Block two tables together using all the rules.
-
-#         Parameters
-#         ----------
-#         left
-#             The left table to block
-#         right
-#             The right table to block
-#         labels
-#             If False, the resulting table will only contain the columns of left and
-#             right. If True, a column of type `array<string>` will be added to the
-#             resulting table indicating which
-#             rules caused each record pair to be blocked.
-
-#             False is faster, because if a pair matches multiple rules we don't
-#             have to care about this. True is slower, because we need to test
-#             every rule, but this is useful for investigating the impact of each
-#             rule.
-
-#         Returns
-#         -------
-#         Table
-#             A table with all the columns of left (with a suffix of `_l`) and right
-#             (with a suffix of `_r`). Possibly with the labels column if `add_labels`
-#             is True.
-#         """
-
-#         def blk(rule: BlockingRule) -> Table:
-#             sub = rule(left, right, on_slow=on_slow, **kwargs)
-#             if labels:
-#                 sub = sub.mutate(blocking_rule=rule.name)
-#             return sub
-
-#         sub_joined = [blk(rule) for rule in self]
-#         if labels:
-#             result = ibis.union(*sub_joined, distinct=False)
-#             result = result.group_by(~s.c("blocking_rule")).agg(
-#                 blocking_rules=_.blocking_rule.collect()
-#             )
-#             result = result.relocate("blocking_rules", after="record_id_r")
-#         else:
-#             result = ibis.union(*sub_joined, distinct=True)
-#         return result
-
-#     def __getitem__(self, name: str) -> BlockingRule:
-#         """Get a rule by name."""
-#         return self._lookup[name]
-
-#     def __iter__(self) -> Iterator[BlockingRule]:
-#         """Iterate over the rules."""
-#         return iter(self._lookup.values())
-
-#     def __len__(self) -> int:
-#         """The number of rules."""
-#         return len(self._lookup)
-
-#     def __repr__(self) -> str:
-#         return f"BlockingRules({tuple(self)})"
