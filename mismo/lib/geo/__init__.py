3bb109276bbbb93b07ee656e8e42a0ee1ddc0a45
Nick Crews <nicholas.b.crews@gmail.com>
2024-02-18 16:54:23 -0900

    ref: move latlon stuff into one module
    I'm about to add address features

--- mismo/lib/geo/__init__.py
+++ mismo/lib/geo/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import annotations
 
-from mismo.lib.geo._block import CoordinateBlocker as CoordinateBlocker
-from mismo.lib.geo._compare import distance_km as distance_km
+from mismo.lib.geo._latlon import CoordinateBlocker as CoordinateBlocker
+from mismo.lib.geo._latlon import distance_km as distance_km
