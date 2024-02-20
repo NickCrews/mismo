4f6eb2da9aa00cc97d8ea80fb2110dfdb1d72dd9
Nick Crews <nicholas.b.crews@gmail.com>
2024-02-19 17:01:09 -0900

    chore: export datasets from toplevel

--- mismo/__init__.py
+++ mismo/__init__.py
@@ -4,4 +4,5 @@ from mismo import block as block
 from mismo import clean as clean
 from mismo import cluster as cluster
 from mismo import compare as compare
+from mismo import datasets as datasets
 from mismo import fs as fs
