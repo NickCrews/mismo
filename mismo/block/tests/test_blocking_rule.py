473c0b69193624035576b30bd8883aab334dd686
Nick Crews <nicholas.b.crews@gmail.com>
2024-02-22 19:44:47 -0900

    test: test naming of BlockingRule more

--- mismo/block/tests/test_blocking_rule.py
+++ mismo/block/tests/test_blocking_rule.py
@@ -11,21 +11,18 @@ from .common import letter_blocked_ids
 
 
 @pytest.mark.parametrize(
-    "condition",
+    "condition,name,expected_name",
     [
-        "letter",
-        ("letter", "letter"),
-        lambda left, right, **kwargs: left.letter == right.letter,
-        lambda left, right, **kwargs: "letter",
-        lambda left, right, **kwargs: (_.letter, _.letter),
-        (_.letter, _.letter),
-        [(_.letter, _.letter)],
+        ("letter", None, "letter"),
+        (_.letter, None, "_.letter"),
+        ("letter", "my_name", "my_name"),
     ],
 )
-def test_blocking_rule(table_factory, t1: it.Table, t2: it.Table, condition):
-    name = "test_rule"
+def test_blocking_rule(
+    table_factory, t1: it.Table, t2: it.Table, condition, name, expected_name
+):
     rule = BlockingRule(condition, name=name)
-    assert rule.get_name() == name
-    assert rule.condition == condition
+    assert rule.get_name() == expected_name
+    assert rule.condition is condition
     expected = letter_blocked_ids(table_factory)
     assert_tables_equal(expected, block_one(t1, t2, rule)["record_id_l", "record_id_r"])
