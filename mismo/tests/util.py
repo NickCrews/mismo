6f1852d2146cab0b66d7e27e075e1ff94c94fa08
Nick Crews <nicholas.b.crews@gmail.com>
2024-02-27 09:48:35 -0900

    test: add optional row sort when comparing tables
    This makes it easier to align the tables with the input test cases.

--- mismo/tests/util.py
+++ mismo/tests/util.py
@@ -4,12 +4,19 @@ from ibis.expr import types as it
 import pandas._testing as tm
 
 
-def assert_tables_equal(left: it.Table, right: it.Table) -> None:
+def assert_tables_equal(
+    left: it.Table,
+    right: it.Table,
+    *,
+    order_by=None,
+) -> None:
     assert dict(left.schema()) == dict(right.schema())
     # need to sort after converting to pandas to avoid
     # https://github.com/ibis-project/ibis/issues/8442
-    left_df = left.to_pandas().sort_values(left.columns, ignore_index=True)
-    right_df = right.to_pandas().sort_values(right.columns, ignore_index=True)
+    if order_by is None:
+        order_by = left.columns
+    left_df = left.to_pandas().sort_values(order_by, ignore_index=True)
+    right_df = right.to_pandas().sort_values(order_by, ignore_index=True)
     try:
         tm.assert_frame_equal(left_df, right_df)
     except AssertionError:
