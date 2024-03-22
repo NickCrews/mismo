8be97ae4ad919760eac251adfcce63835a4d9535
Nick Crews <nicholas.b.crews@gmail.com>
2024-03-22 12:57:18 -0800

    address: migrate from AddressLevelComparer to AddressesDimension
    If I tried to split up the functionality here into too many small components, the config to make everything
    get plumbed together was getting out of hand.
    
    I'd rather supply the basic building block functions, but then just have one opinionated higher level interface. If people want more customization, they can do it themselves.

--- mismo/lib/geo/__init__.py
+++ mismo/lib/geo/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from ._address import AddressesDimension as AddressesDimension
+from ._address import normalize_address as normalize_address
 from ._census import us_census_geocode as us_census_geocode
 from ._latlon import CoordinateBlocker as CoordinateBlocker
 from ._latlon import distance_km as distance_km
--- mismo/lib/geo/_address.py
+++ mismo/lib/geo/_address.py
@@ -1,94 +1,12 @@
 from __future__ import annotations
 
-import dataclasses
-from typing import Callable
-
 import ibis
-from ibis.common.deferred import Deferred
 from ibis.expr import types as it
 
 from mismo import _util
 from mismo.compare import LevelComparer, compare
 from mismo.lib.geo._latlon import distance_km
-
-
-@dataclasses.dataclass
-class AddressLevelComparer:
-    """An opinionated [LevelComparer][mismo.compare.LevelComparer] for street addresses.
-
-    This labels record pairs with a level of similarity based on how well
-    the addresses match.
-
-    This assumes that you have already normalized the addresses,
-    eg you have already transformed all of
-    "New York", "NY", and  " NEW YORK" to the same value.
-    """
-
-    def __init__(
-        self,
-        column_left: str | Deferred | Callable[[it.Table], it.StructColumn],
-        column_right: str | Deferred | Callable[[it.Table], it.StructColumn],
-        name: str = "address_agreement",
-    ):
-        """Compare two tables on the specified address columns.
-
-        An address column is expected to be a Struct of the type
-        `struct<
-            street1: string,
-            street2: string,  # eg "Apt 3"
-            city: string,
-            state: string,
-            postal_code: string,  # zipcode in the US
-            country: string,
-        >`.
-
-        Parameters
-        ----------
-        column_left:
-            The column in the left table containing the name struct.
-        column_right:
-            The column in the right table containing the name struct.
-        """
-        self.column_left = column_left
-        self.column_right = column_right
-        self.name = name
-
-    def __call__(self, t: it.Table) -> it.Table:
-        al = _util.get_column(t, self.column_left, on_many="struct")
-        ar = _util.get_column(t, self.column_right, on_many="struct")
-        if "latitude" in al.type().names:
-            within_100km_levels = [
-                (
-                    "within_100km",
-                    distance_km(
-                        lat1=al.latitude,
-                        lon1=al.longitude,
-                        lat2=ar.latitude,
-                        lon2=ar.longitude,
-                    )
-                    <= 100,
-                ),
-            ]
-        else:
-            within_100km_levels = []
-        levels = [
-            (
-                "null",
-                ibis.or_(
-                    _util.struct_isnull(
-                        al, how="any", fields=["street1", "city", "state"]
-                    ),
-                    _util.struct_isnull(
-                        ar, how="any", fields=["street1", "city", "state"]
-                    ),
-                ),
-            ),
-            ("street1_and_city_or_postal_code", same_address_for_mailing(al, ar)),
-            ("same_region", same_region(al, ar)),
-            *within_100km_levels,
-            ("same_state", al.state == ar.state),
-        ]
-        return compare(t, LevelComparer(self.name, levels))
+from mismo.text import rare_terms
 
 
 def same_region(
@@ -138,4 +56,162 @@ def same_address_for_mailing(
         ibis.or_(
             address1.city == address2.city, address1.postal_code == address2.postal_code
         ),
+    )
+
+
+def normalize_address(address: it.StructValue) -> it.StructValue:
+    """Normalize an address to uppercase, and remove leading and trailing whitespace.
+
+    Parameters
+    ----------
+    address : it.StructValue
+        The address.
+
+    Returns
+    -------
+    normalized : it.StructValue
+        The normalized address.
+    """
+    return ibis.struct(
+        {
+            "street1": address.street1.upper().strip(),
+            "street2": address.street2.upper().strip(),
+            "city": address.city.upper().strip(),
+            "state": address.state.upper().strip(),
+            "postal_code": address.postal_code.upper().strip(),
+            "country": address.country.upper().strip(),
+        }
+    )
+
+
+class AddressesDimension:
+    """Preps, blocks, and compares based on array<address> columns.
+
+    An address is a Struct of the type
+    `struct<
+        street1: string,
+        street2: string,  # eg "Apt 3"
+        city: string,
+        state: string,
+        postal_code: string,  # zipcode in the US
+        country: string,
+    >`.
+    This operates on columns of type `array<address>`. In other words,
+    it is useful for comparing eg people, who might have multiple addresses,
+    and they are the same person if any of their addresses match.
+
+    Parameters
+    ----------
+    column_left:
+        The column in the left table containing the name struct.
+    column_right:
+        The column in the right table containing the name struct.
+    """
+
+    def __init__(
+        self,
+        column: str,
+        *,
+        name: str = "addresses",
+        column_normed: str = "{column}_normed",
+        column_tokens: str = "{column}_tokens",
+        column_keywords: str = "{column}_keywords",
+    ):
+        self.column = column
+        self.name = name
+        self.column_normed = column_normed.format(column=column)
+        self.column_tokens = column_tokens.format(column=column)
+        self.column_keywords = column_keywords.format(column=column)
+
+    def prep(self, t: it.Table) -> it.Table:
+        """Prepares the table for blocking, adding normalized and tokenized columns."""
+        addrs = t[self.column]
+        addrs_normed = addrs.map(normalize_address)
+        t = t.mutate({self.column_normed: addrs_normed})
+        tokens = (
+            t[self.column_normed]
+            .map(lambda address: address_tokens(address))
+            .flatten()
+            .unique()
+        )
+        t = t.mutate({self.column_tokens: tokens})
+        # since we need the .execute below, might as well .cache()
+        # here to avoid re-execution
+        t = t.cache()
+        rt = rare_terms(t[self.column_tokens], max_records_frac=0.01).term
+        # need this to avoid https://stackoverflow.com/questions/77559936/how-to-implementlist-filterarray-elem-elem-in-column-in-other-table
+        # see https://github.com/NickCrews/mismo/issues/32
+        rt = rt.execute()
+        keywords = t[self.column_tokens].filter(lambda tok: tok.isin(rt))
+        t = t.mutate({self.column_keywords: keywords})
+        return t
+
+    def compare(self, t: it.Table) -> it.Table:
+        al = t[self.column_normed + "_l"]
+        ar = t[self.column_normed + "_r"]
+        if "latitude" in al.type().names:
+            within_100km_levels = [
+                (
+                    "within_100km",
+                    distance_km(
+                        lat1=al.latitude,
+                        lon1=al.longitude,
+                        lat2=ar.latitude,
+                        lon2=ar.longitude,
+                    )
+                    <= 100,
+                ),
+            ]
+        else:
+            within_100km_levels = []
+        levels = [
+            (
+                "null",
+                ibis.or_(
+                    _util.struct_isnull(
+                        al, how="any", fields=["street1", "city", "state"]
+                    ),
+                    _util.struct_isnull(
+                        ar, how="any", fields=["street1", "city", "state"]
+                    ),
+                ),
+            ),
+            ("street1_and_city_or_postal_code", same_address_for_mailing(al, ar)),
+            ("same_region", same_region(al, ar)),
+            *within_100km_levels,
+            ("same_state", al.state == ar.state),
+        ]
+        return compare(t, LevelComparer(self.name, levels))
+
+
+def address_tokens(address: it.StructValue) -> it.ArrayColumn:
+    """Extract keywords from an address.
+
+    Parameters
+    ----------
+    address : it.StructValue
+        The address.
+
+    Returns
+    -------
+    keywords : it.ArrayColumn
+        The keywords in the address.
+    """
+
+    def tokens(s: it.StringValue) -> it.ArrayColumn:
+        return s.re_split(r"\s+")
+
+    return (
+        ibis.array(
+            [
+                tokens(address.street1),
+                tokens(address.street2),
+                tokens(address.city),
+                tokens(address.state),
+                tokens(address.postal_code),
+                tokens(address.country),
+            ]
+        )
+        .flatten()
+        .unique()
     )
