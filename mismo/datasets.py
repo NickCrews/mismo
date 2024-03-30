a8ed2ea42c8606e3bf9f405fc6e5aef37ca1ee87
Olivier Binette <olivier@Oliviers-Air.lan>
2024-03-30 12:28:03 -0400

    Add load_rldata functions and tests

--- mismo/datasets.py
+++ mismo/datasets.py
@@ -12,6 +12,8 @@ __all__ = [
     "load_febrl2",
     "load_febrl3",
     "load_patents",
+    "load_rldata500",
+    "load_rldata10000",
 ]
 
 _DATASETS_DIR = Path(__file__).parent / "_data/_datasets"
@@ -126,3 +128,161 @@ def load_patents(backend: ibis.BaseBackend | None = None) -> ir.Table:
     # parallel=False kwarg, but I'd rather just have them sorted
     # by record_id
     return backend.read_csv(path).order_by("record_id").cache()
+
+
+def rldata_schema() -> dict:
+    return {
+        "record_id": "int64",
+        "label_true": "int64",
+        "fname_c1": "string",
+        "fname_c2": "string",
+        "lname_c1": "string",
+        "lname_c2": "string",
+        "by": "int64",
+        "bm": "int64",
+        "bd": "int64",
+    }
+
+
+def load_rldata500(backend: ibis.BaseBackend | None = None) -> ir.Table:
+    """Synthetic personal information dataset with 500 rows
+
+    This is a synthetic dataset with noisy names and dates of birth, with the task being
+    to determine which rows represent the same person. 10% of the records are duplicates
+    of existing ones, and the level of noise is low. The dataset can be deduplicated with 90%+
+    precision and recall using simple linkage rules. It is often used as a
+    sanity check for computational efficiency and disambiguation accuracy.
+
+    This comes from the
+    [RecordLinkage R package](https://cran.r-project.org/web/packages/RecordLinkage/index.html)
+    and was generated using the data generation component of
+    [Febrl (Freely Extensible Biomedical Record Linkage)](https://sourceforge.net/projects/febrl/).
+
+    Returns
+    -------
+    Table
+        An Ibis Table with the following schema:
+
+        - record_id: int64
+          A unique ID for each row in the table.
+        - label_true: int64
+          The manually labeled, true ID of the inventor.
+        - fname_c1: string
+          First component of the first name.
+        - fname_c2: string
+          Second component of the first name (mostly NULL values)
+        - lname_c1: string
+          First component of the last name.
+        - lname_c2: string
+          Second component of the last name (mostly NULL values).
+        - by: int64
+          Birth year
+        - bm: int64
+          Birth month
+        - bd: int64
+          Birth day
+
+    Examples
+    --------
+    >>> load_rldata500()
+    ┏━━━━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━┳━━━━━━━┳━━━━━━━┓
+    ┃ record_id ┃ label_true ┃ fname_c1 ┃ fname_c2 ┃ lname_c1 ┃ lname_c2 ┃ by    ┃ bm    ┃ bd    ┃
+    ┡━━━━━━━━━━━╇━━━━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━╇━━━━━━━╇━━━━━━━┩
+    │ int64     │ int64      │ string   │ string   │ string   │ string   │ int64 │ int64 │ int64 │
+    ├───────────┼────────────┼──────────┼──────────┼──────────┼──────────┼───────┼───────┼───────┤
+    │         0 │         34 │ CARSTEN  │ NULL     │ MEIER    │ NULL     │  1949 │     7 │    22 │
+    │         1 │         51 │ GERD     │ NULL     │ BAUER    │ NULL     │  1968 │     7 │    27 │
+    │         2 │        115 │ ROBERT   │ NULL     │ HARTMANN │ NULL     │  1930 │     4 │    30 │
+    │         3 │        189 │ STEFAN   │ NULL     │ WOLFF    │ NULL     │  1957 │     9 │     2 │
+    │         4 │         72 │ RALF     │ NULL     │ KRUEGER  │ NULL     │  1966 │     1 │    13 │
+    │         5 │        142 │ JUERGEN  │ NULL     │ FRANKE   │ NULL     │  1929 │     7 │     4 │
+    │         6 │        162 │ GERD     │ NULL     │ SCHAEFER │ NULL     │  1967 │     8 │     1 │
+    │         7 │         48 │ UWE      │ NULL     │ MEIER    │ NULL     │  1942 │     9 │    20 │
+    │         8 │        133 │ DANIEL   │ NULL     │ SCHMIDT  │ NULL     │  1978 │     3 │     4 │
+    │         9 │        190 │ MICHAEL  │ NULL     │ HAHN     │ NULL     │  1971 │     2 │    27 │
+    │         … │          … │ …        │ …        │ …        │ …        │     … │     … │     … │
+    └───────────┴────────────┴──────────┴──────────┴──────────┴──────────┴───────┴───────┴───────┘
+    """  # noqa: E501
+    path = _DATASETS_DIR / "rldata/RLdata500.csv"
+    schema = rldata_schema()
+    if backend is None:
+        backend = ibis
+    return (
+        backend.read_csv(path)
+        .cast(schema)
+        .select(*schema.keys())
+        .order_by("record_id")
+        .cache()
+    )
+
+
+def load_rldata10000(backend: ibis.BaseBackend | None = None) -> ir.Table:
+    """Synthetic personal information dataset with 10000 rows
+
+    This is a synthetic dataset with noisy names and dates of birth, with the task being
+    to determine which rows represent the same person. 10% of the records are duplicates
+    of existing ones, and the level of noise is low. The dataset can be deduplicated with 90%+
+    precision and recall using simple linkage rules. It is often used as a
+    sanity check for computational efficiency and disambiguation accuracy.
+
+    This comes from the
+    [RecordLinkage R package](https://cran.r-project.org/web/packages/RecordLinkage/index.html)
+    and was generated using the data generation component of
+    [Febrl (Freely Extensible Biomedical Record Linkage)](https://sourceforge.net/projects/febrl/).
+
+    Returns
+    -------
+    Table
+        An Ibis Table with the following schema:
+
+        - record_id: int64
+          A unique ID for each row in the table.
+        - label_true: int64
+          The manually labeled, true ID of the inventor.
+        - fname_c1: string
+          First component of the first name.
+        - fname_c2: string
+          Second component of the first name (mostly NULL values)
+        - lname_c1: string
+          First component of the last name.
+        - lname_c2: string
+          Second component of the last name (mostly NULL values).
+        - by: int64
+          Birth year
+        - bm: int64
+          Birth month
+        - bd: int64
+          Birth day
+
+    Examples
+    --------
+    >>> load_rldata10000()
+    ┏━━━━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━┳━━━━━━━┳━━━━━━━┓
+    ┃ record_id ┃ label_true ┃ fname_c1 ┃ fname_c2 ┃ lname_c1   ┃ lname_c2 ┃ by    ┃ bm    ┃ bd    ┃
+    ┡━━━━━━━━━━━╇━━━━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━╇━━━━━━━╇━━━━━━━┩
+    │ int64     │ int64      │ string   │ string   │ string     │ string   │ int64 │ int64 │ int64 │
+    ├───────────┼────────────┼──────────┼──────────┼────────────┼──────────┼───────┼───────┼───────┤
+    │         0 │       3606 │ FRANK    │ NULL     │ MUELLER    │ NULL     │  1967 │     9 │    27 │
+    │         1 │       2560 │ MARTIN   │ NULL     │ SCHWARZ    │ NULL     │  1967 │     2 │    17 │
+    │         2 │       3892 │ HERBERT  │ NULL     │ ZIMMERMANN │ NULL     │  1961 │    11 │     6 │
+    │         3 │        329 │ HANS     │ NULL     │ SCHMITT    │ NULL     │  1945 │     8 │    14 │
+    │         4 │       1994 │ UWE      │ NULL     │ KELLER     │ NULL     │  2000 │     7 │     5 │
+    │         5 │       2330 │ DANIEL   │ NULL     │ HEINRICH   │ NULL     │  1967 │     5 │     6 │
+    │         6 │       4420 │ MARTIN   │ NULL     │ ZIMMERMANN │ NULL     │  1982 │    11 │     2 │
+    │         7 │       2534 │ ANDREAS  │ BENJAMIN │ BERGMANN   │ NULL     │  1989 │     9 │    13 │
+    │         8 │       3076 │ DIETER   │ NULL     │ SCHUSTER   │ NULL     │  1974 │     4 │    19 │
+    │         9 │       4305 │ MANFRED  │ NULL     │ SCHMIDT    │ NULL     │  1979 │     7 │    11 │
+    │         … │          … │ …        │ …        │ …          │ …        │     … │     … │     … │
+    └───────────┴────────────┴──────────┴──────────┴────────────┴──────────┴───────┴───────┴───────┘
+    """  # noqa: E501
+    path = _DATASETS_DIR / "rldata/RLdata10000.csv"
+    schema = rldata_schema()
+    if backend is None:
+        backend = ibis
+    return (
+        backend.read_csv(path)
+        .cast(schema)
+        .select(*schema.keys())
+        .order_by("record_id")
+        .cache()
+    )
