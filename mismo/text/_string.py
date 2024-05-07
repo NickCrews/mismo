0302ab9478ac8113ce54f0be0fcaaddee80b35ae
Jimmy Stammers <jimmy.stammers@gmail.com>
2024-05-07 12:22:24 +0000

    simplify ratio calc

--- mismo/text/_string.py
+++ mismo/text/_string.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
 from ibis.expr import types as ir
+import ibis
 
 
 def levenshtein_ratio(s1: ir.StringColumn, s2: ir.StringColumn) -> ir.FloatingColumn:
     """Computes the similarity ratio between two strings
     using the Levenshtein distance.
 
-    This is defined as `(1 - lcost/lsum)` where `lcost` is the
-    cost of replacing characters in one string to match the other
-    and `lsum` is the total length of the two strings
+    This is defined as `(lensum - ldist)/lensum` where `lensum` is the
+    maximum length of the two strings and ldist is the number of edits required 
+    to transform one string into the other
 
     Parameters
     ----------
@@ -24,7 +25,6 @@ def levenshtein_ratio(s1: ir.StringColumn, s2: ir.StringColumn) -> ir.FloatingCo
     lev_ratio:
         The ratio of the Levenshtein edit cost to the total string length
     """
-
-    lev_cost = 2 * s1.levenshtein(s2)
-    len_sum = s1.length() + s2.length()
-    return 1 - lev_cost / len_sum
+    lensum = ibis.greatest(s1.length(), s2.length())
+    ldist = s1.levenshtein(s2)
+    return (lensum - ldist)/lensum
