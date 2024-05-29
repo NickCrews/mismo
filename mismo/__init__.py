c67990332e3a180271c0819292964b9e30f9ce87
Nick Crews <nicholas.b.crews@gmail.com>
2024-05-29 13:15:18 -0800

    fixup: update example with new APIs

--- mismo/__init__.py
+++ mismo/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from mismo import arrays as arrays
 from mismo import block as block
 from mismo import cluster as cluster
 from mismo import compare as compare
@@ -7,6 +8,7 @@ from mismo import eda as eda
 from mismo import fs as fs
 from mismo import lib as lib
 from mismo import playdata as playdata
+from mismo import sets as sets
 from mismo import text as text
 from mismo import vector as vector
 from mismo._datasets import Datasets as Datasets
