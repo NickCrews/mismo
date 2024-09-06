cb8185c9a1d77861a92ddb33b76cba774019c773
Nick Crews <nicholas.b.crews@gmail.com>
2024-09-06 12:38:03 -0800

    NameMatchLevel: add TYPO level

--- mismo/lib/name/__init__.py
+++ mismo/lib/name/__init__.py
@@ -4,6 +4,7 @@ from ._blocker import NameBlocker as NameBlocker
 from ._clean import normalize_name as normalize_name
 from ._compare import NameComparer as NameComparer
 from ._compare import NameMatchLevel as NameMatchLevel
+from ._compare import equal_forgiving_typo as equal_forgiving_typo
 from ._dimension import NameDimension as NameDimension
 from ._nicknames import are_aliases as are_aliases
 from ._nicknames import is_nickname_for as is_nickname_for
