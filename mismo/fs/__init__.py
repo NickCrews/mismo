002ca8723a6b821a64ee049e38fbfe17cdb13b85
Nick Crews <nicholas.b.crews@gmail.com>
2024-10-28 11:22:50 -0800

    feat(fs): add train_using_pairs()

--- mismo/fs/__init__.py
+++ mismo/fs/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from ._plot import plot_weights as plot_weights
 from ._train import train_using_labels as train_using_labels
+from ._train import train_using_pairs as train_using_pairs
 from ._train_em import train_using_em as train_using_em
 from ._weights import ComparerWeights as ComparerWeights
 from ._weights import LevelWeights as LevelWeights
