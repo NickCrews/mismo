6ff83de8b76d5db8d2799b2bf021a311731efbf7
Nick Crews <nicholas.b.crews@gmail.com>
2025-01-22 15:17:19 -0900

    MatchCounts: rename to LinkCounts
    I think "Match" implies that any linked records are CONFIRMED as true matches.
    But this might not be the case, you might generate a bunch of candidate links, and then prune them down further into true matches.
    I think "Link" is more neutral to just mean "these records are associated"

--- mismo/_find/_find_results.py
+++ mismo/_find/_find_results.py
@@ -16,35 +16,44 @@ if TYPE_CHECKING:
     import altair as alt
 
 
-class MatchCountsTable(_util.TableWrapper):
-    """A table representing the number of records by the number of matches.
+class LinkCountsTable(_util.TableWrapper):
+    """A table representing the number of records binned by number of links.
 
-    eg "There were 700 records with 0 matches, 300 with 1 match, 20 with 2 matches, ..."
+    eg "There were 700 records with 0 links, 300 with 1 link, 20 with 2 links, ..."
     """
 
     n_records: ir.IntegerColumn
     """The number of records."""
-    n_matches: ir.IntegerColumn
-    """The number of matches."""
+    n_links: ir.IntegerColumn
+    """The number of links."""
+
+    def __init__(self, t):
+        if set(t.columns) != {"n_records", "n_links"}:
+            raise ValueError(
+                "LinkCountsTable must have exactly columns 'n_records' and 'n_links'"
+            )
+        super().__init__(t)
 
     def chart(self) -> alt.Chart:
-        """A bar chart of the number of records by the number of matches."""
+        """A bar chart of the number of records by the number of links."""
         import altair as alt
 
         n_title = "Number of Records"
-        key_title = "Number of Matches"
-        mins = self.order_by(_.n_matches.asc()).limit(2).execute()
+        key_title = "Number of Links"
+        mins = self.order_by(_.n_links.asc()).limit(2).execute()
         if len(mins) > 1:
-            subtitle = f"eg '{mins.n_records[0]:,} records had {mins.n_matches[0]} matches, {mins.n_records[1]:,} had {mins.n_matches[1]} matches, ...'"  # noqa: E501
+            subtitle = f"eg '{mins.n_records[0]:,} records had {mins.n_links[0]} links, {mins.n_records[1]:,} had {mins.n_links[1]} links, ...'"  # noqa: E501
         elif len(mins) == 1:
-            subtitle = f"eg '{mins.n_records[0]:,} records had {mins.n_matches[0]} matches', ..."  # noqa: E501
+            subtitle = (
+                f"eg '{mins.n_records[0]:,} records had {mins.n_links[0]} links', ..."  # noqa: E501
+            )
         else:
-            subtitle = "eg 'there were 1000 records with 0 matches, 500 with 1 match, 100 with 2 matches, ...'"  # noqa: E501
+            subtitle = "eg 'there were 1000 records with 0 links, 500 with 1 link, 100 with 2 links, ...'"  # noqa: E501
         chart = (
             alt.Chart(self)
             .properties(
                 title=alt.TitleParams(
-                    "Number of Records by Match Count",
+                    "Number of Records by Link Count",
                     subtitle=subtitle,
                     anchor="middle",
                 ),
@@ -54,7 +63,7 @@ class MatchCountsTable(_util.TableWrapper):
             .encode(
                 # if we ever change this sorting, keep the subtitle example
                 # to be in sync of the same order as the bars go left to right.
-                alt.X("n_matches:O", title=key_title, sort="x"),
+                alt.X("n_links:O", title=key_title, sort="x"),
                 alt.Y(
                     "n_records:Q",
                     title=n_title,
@@ -62,7 +71,7 @@ class MatchCountsTable(_util.TableWrapper):
                 ),
                 tooltip=[
                     alt.Tooltip("n_records:Q", title=n_title, format=","),
-                    alt.Tooltip("n_matches:O", title=key_title),
+                    alt.Tooltip("n_links:O", title=key_title),
                 ],
             )
         )
@@ -71,21 +80,21 @@ class MatchCountsTable(_util.TableWrapper):
 
 class LabeledTable(ibis.Table):
     """
-    A table with a `record_ids` column added of the match(es) from the other table.
+    A table with a `record_ids` column added of the link(s) to the other table.
 
     This represents one of the input tables, augmented with another column
-    that points to the best match(es) in the other table.
+    that points to the best link(s) in the other table.
     """
 
     record_id: ir.Column
     """The record_id of this table."""
     record_ids: ir.ArrayColumn
-    """The record_ids of the matches in the other table. Never NULL, if no matches, empty array."""  # noqa: E501
+    """The record_ids of the links in the other table. Never NULL, if no links, empty array."""  # noqa: E501
 
 
 class LinkTable(ibis.Table):
     """
-    A table with a `record_id_l` and `record_id_r` column, representing matching pairs.
+    A table with a `record_id_l` and `record_id_r` column, representing linked pairs.
     """
 
     record_id_l: ir.Column
@@ -98,10 +107,10 @@ class FindResults:
     For example, you have an existing database that is clean, and you want
     to ingest some new, messy data.
     For each of the records in the new, messy data, you want to find
-    the matches in the existing database.
+    the linked records in the existing database.
 
     The `haystack` is the existing database, the `needle` is the new data,
-    and the `links` are the pairs of matches between the two.
+    and the `links` are the relationships between the two.
     """
 
     def __init__(
@@ -116,7 +125,8 @@ class FindResults:
         needle
             The table being searched for. Must contain a `record_id` column.
         links
-            The pairs of matches between the needle and the haystack.
+            Table of (recod_id_l, record_id_r) representing links between
+            the needle and the haystack.
 
             !!! warning
             `record_id_l` is the record_id of the haystack,
@@ -135,7 +145,9 @@ class FindResults:
         return self._needle
 
     def links(self) -> ir.Table:
-        """The pairs of matches between the needle and the haystack."""
+        """
+        The (record_id_l, record_id_r) pairs referencing the needle and the haystack.
+        """
         return self._links
 
     def to_parquets(self, directory: str | Path, /) -> None:
@@ -163,7 +175,7 @@ class FindResults:
         )
 
     def needle_labeled(self, *, name: str = "record_ids") -> LabeledTable:
-        """The needle, with a `record_ids` column added of matches from the haystack."""
+        """The needle, with a `record_ids` column added of links from the haystack."""
         n = self.needle()
         if name in n.columns:
             warnings.warn(f"Column '{name}' will be overwritten in needle.")
@@ -177,7 +189,7 @@ class FindResults:
         return _util.join_lookup(n, lookup, "record_id", defaults={name: []})
 
     def needle_labeled_none(self) -> LabeledTable:
-        """The subset of needle_labeled with no matches."""
+        """The subset of needle_labeled with no links."""
         return self.needle_labeled().filter(_.record_ids.length() == 0)
 
     def needle_labeled_single(
@@ -186,7 +198,7 @@ class FindResults:
         name: str = "record_id",
         format: Literal["single", "array"] = "single",
     ) -> ir.Table:
-        """The subset of needle_labeled with exactly one match.
+        """The subset of needle_labeled with exactly one link.
 
         Parameters
         ----------
@@ -205,18 +217,18 @@ class FindResults:
             raise ValueError(f"format must be 'single' or 'array', got {format:!r}")
 
     def needle_labeled_many(self) -> LabeledTable:
-        """The subset of needle_labeled with more than one match."""
+        """The subset of needle_labeled with more than one link."""
         return self.needle_labeled().filter(_.record_ids.length() > 1)
 
-    def needle_match_counts(self) -> MatchCountsTable:
+    def needle_link_counts(self) -> LinkCountsTable:
         """
-        A histogram of the number of records in the needle, binned by number of matches.
+        A histogram of the number of records in the needle, binned by number of links.
 
-        e.g. "There were 700 records with 0 matches, 300 with 1 match,
-        20 with 2 matches, ..."
+        e.g. "There were 700 records with 0 links, 300 with 1 link,
+        20 with 2 links, ..."
         """
-        n_matches_by_record = self.links().record_id_r.value_counts(name="n_matches")
-        counts = n_matches_by_record.group_by("n_matches").agg(n_records=_.count())
+        n_links_by_record = self.links().record_id_r.value_counts(name="n_links")
+        counts = n_links_by_record.group_by("n_links").agg(n_records=_.count())
         n_not_linked = (
             self.needle()
             .select("record_id")
@@ -227,15 +239,15 @@ class FindResults:
         extra = (
             n_not_linked.name("n_records")
             .as_table()
-            .mutate(n_matches=0)
+            .mutate(n_links=0)
             .cast(counts.schema())
         )
         counts = counts.union(extra)
-        counts = counts.order_by(_.n_matches)
-        return MatchCountsTable(counts)
+        counts = counts.order_by(_.n_links)
+        return LinkCountsTable(counts)
 
     def haystack_labeled(self, *, name: str = "record_ids") -> LabeledTable:
-        """The haystack, with a `record_ids` column added of matches from the needle."""
+        """The haystack, with a `record_ids` column added of links from the needle."""
         h = self.haystack()
         if name in h.columns:
             warnings.warn(f"Column '{name}' will be overwritten in haystack.")
@@ -249,7 +261,7 @@ class FindResults:
         return _util.join_lookup(h, lookup, "record_id", defaults={name: []})
 
     def haystack_labeled_none(self) -> LabeledTable:
-        """The subset of haystack with no matches."""
+        """The subset of haystack with no links."""
         return self.haystack_labeled().filter(_.record_ids.length() == 0)
 
     def haystack_labeled_single(
@@ -258,7 +270,7 @@ class FindResults:
         name: str = "record_id",
         format: Literal["single", "array"] = "single",
     ) -> ir.Table:
-        """The subset of haystack_labeled with exactly one match.
+        """The subset of haystack_labeled with exactly one link.
 
         Parameters
         ----------
@@ -277,18 +289,18 @@ class FindResults:
             raise ValueError(f"format must be 'single' or 'array', got {format:!r}")
 
     def haystack_labeled_many(self) -> LabeledTable:
-        """The subset of haystack with more than one match."""
+        """The subset of haystack with more than one link."""
         return self.haystack_labeled().filter(_.record_ids.length() > 1)
 
-    def haystack_match_counts(self) -> MatchCountsTable:
+    def haystack_link_counts(self) -> LinkCountsTable:
         """
-        A histogram of the records in the haystack, binned by number of matches.
+        A histogram of the records in the haystack, binned by number of links.
 
-        e.g. "There were 700 records with 0 matches, 300 with 1 match,
-        20 with 2 matches, ..."
+        e.g. "There were 700 records with 0 links, 300 with 1 link,
+        20 with 2 links, ..."
         """
-        n_matches_by_record = self.links().record_id_l.value_counts(name="n_matches")
-        counts = n_matches_by_record.group_by("n_matches").agg(n_records=_.count())
+        n_links_by_record = self.links().record_id_l.value_counts(name="n_links")
+        counts = n_links_by_record.group_by("n_links").agg(n_records=_.count())
         n_not_linked = (
             self.haystack()
             .select("record_id")
@@ -299,12 +311,12 @@ class FindResults:
         extra = (
             n_not_linked.name("n_records")
             .as_table()
-            .mutate(n_matches=0)
+            .mutate(n_links=0)
             .cast(counts.schema())
         )
         counts = counts.union(extra)
-        counts = counts.order_by(_.n_matches)
-        return MatchCountsTable(counts)
+        counts = counts.order_by(_.n_links)
+        return LinkCountsTable(counts)
 
     @functools.cache
     def __str__(self) -> str:
