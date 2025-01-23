4807867a6011b5ce0a5b28589ead161c5dad6562
Nick Crews <nicholas.b.crews@gmail.com>
2025-01-23 10:56:10 -0900

    ref: rename types/_linked_table.py to _linkage.py
    it's just shorted

--- mismo/types/__init__.py
+++ mismo/types/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from mismo.types._diff import Diff as Diff
-from mismo.types._linked_table import Linkage as Linkage
-from mismo.types._linked_table import LinkCountsTable as LinkCountsTable
-from mismo.types._linked_table import LinkedTable as LinkedTable
+from mismo.types._linkage import Linkage as Linkage
+from mismo.types._linkage import LinkCountsTable as LinkCountsTable
+from mismo.types._linkage import LinkedTable as LinkedTable
 from mismo.types._updates import Updates as Updates
