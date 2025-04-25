fc65e36734ed70999a8d6eb7afb903b3308ed527
Nick Crews <nicholas.b.crews@gmail.com>
2025-04-25 12:23:53 -0700

    f me I have no idea what I'm trying to do

---
+++ mismo/linkage/_dimension.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+import typing
+
+import ibis
+from ibis import ir
+
+from mismo.types._links_table import LinksTable
+
+
+@typing.runtime_checkable
+class Dimension(typing.Protocol):
+    def __pre_link__(
+        self, left: ibis.Table, right: ibis.Table
+    ) -> tuple[ibis.Table, ibis.Table]:
+        raise NotImplementedError("Pre-linker must implement __pre_join__ method")
+
+    def __join_condition__(
+        self, left: ibis.Table, right: ibis.Table
+    ) -> ir.BooleanValue | bool:
+        pass
+
+    def __compare__(self, links: LinksTable) -> LinksTable:
+        pass
+
+
+class DimensionsLinker:
+    def __init__(self, dimensions: Iterable[Dimension]) -> None:
+        self.dimension = tuple(dimensions)
+
+    def __pre_link__(
+        self, left: ibis.Table, right: ibis.Table
+    ) -> tuple[ibis.Table, ibis.Table]:
+        for linker in self.dimension:
+            left, right = linker.__pre_link__(left, right)
+        return left, right
+
+    def __join_condition__(
+        self, left: ibis.Table, right: ibis.Table
+    ) -> ibis.ir.BooleanValue | bool:
+        clauses = [linker.__join_condition__(left, right) for linker in self.dimension]
+        clauses = [clause for clause in clauses if clause is not True]
+        return ibis.and_(*clauses)
+
+    def __compare__(self, links: LinksTable) -> LinksTable:
+        for linker in self.dimension:
+            links = linker.__compare__(links)
+        return links
