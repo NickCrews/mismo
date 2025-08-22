f4e9a119db592116130c6a2d943ebfc73777603a
Nick Crews <nicholas.b.crews@gmail.com>
2025-08-22 15:06:05 -0600

    fix name of HistSpec in CountsTable

--- mismo/_counts_table.py
+++ mismo/_counts_table.py
@@ -34,7 +34,7 @@ class CountsTable(TableWrapper):
     """The column containing the count."""
 
     # This MUST be set in subclasses
-    _KEY_COUNTS_SPEC: _HistSpec
+    _HIST_SPEC: _HistSpec
 
     @functools.cache
     def n_total(self) -> int:
@@ -45,20 +45,20 @@ class CountsTable(TableWrapper):
     def chart(self, *, n_most_common: int = 50, n_least_common: int = 10) -> alt.Chart:
         return _counts_chart(
             self,
-            hist_spec=self._KEY_COUNTS_SPEC,
+            hist_spec=self._HIST_SPEC,
             n_most_common=n_most_common,
             n_least_common=n_least_common,
         )
 
 
 class KeyCountsTable(CountsTable):
-    _KEY_COUNTS_SPEC = _HistSpec(
+    _HIST_SPEC = _HistSpec(
         "Number of Records", "Number of Records by Key", "{n_total:_} Total Records"
     )
 
 
 class PairCountsTable(CountsTable):
-    _PAIR_COUNTS_SPEC = _HistSpec(
+    _HIST_SPEC = _HistSpec(
         "Number of Pairs", "Number of Pairs by Key", "{n_total:_} Total Pairs"
     )
 
