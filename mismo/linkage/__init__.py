87d53c22a46592a02f73acc8d5b8fa169cc49356
Nick Crews <nicholas.b.crews@gmail.com>
2026-02-01 09:18:26 -0900

    chore: remove unused linkage.union and linkage.intersection
    The model of combining Linkages I think is the wrong abstraction. I think this actually happen at the level of combining linkers.

--- mismo/linkage/__init__.py
+++ mismo/linkage/__init__.py
@@ -1,11 +1,5 @@
 from __future__ import annotations
 
-from mismo.linkage._combine import difference as difference
-from mismo.linkage._combine import intersect as intersect
-from mismo.linkage._combine import (
-    unify_links_min_intersection as unify_links_min_intersection,
-)
-from mismo.linkage._combine import union as union
 from mismo.linkage._linkage import Linkage as Linkage
 from mismo.linkage._linkage import filter_links as filter_links
 from mismo.linkage._sample import sample_all_links as sample_all_links
